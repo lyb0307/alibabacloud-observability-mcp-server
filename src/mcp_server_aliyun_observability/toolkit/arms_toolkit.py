30579f1eee0572ed8568030e7a5588a9a213d742
bojackker <15797893077@163.com>
2025-04-24 19:51:52 +0800

    feat:profiling mcp tool

--- src/mcp_server_aliyun_observability/toolkit/arms_toolkit.py
+++ src/mcp_server_aliyun_observability/toolkit/arms_toolkit.py
@@ -2,16 +2,22 @@ import logging
 from typing import Any
 
 from alibabacloud_arms20190808.client import Client as ArmsClient
+from alibabacloud_sls20201230.client import Client
 from alibabacloud_arms20190808.models import (
-    SearchTraceAppByPageRequest, SearchTraceAppByPageResponse,
-    SearchTraceAppByPageResponseBodyPageBean)
+    SearchTraceAppByPageRequest,
+    SearchTraceAppByPageResponse,
+    SearchTraceAppByPageResponseBodyPageBean,
+)
+from alibabacloud_tea_util import models as util_models
+from alibabacloud_sls20201230.models import CallAiToolsRequest, CallAiToolsResponse
 from mcp.server.fastmcp import Context, FastMCP
 from pydantic import Field
-from tenacity import (retry, retry_if_exception_type, stop_after_attempt,
-                      wait_fixed)
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 from mcp_server_aliyun_observability.utils import (
-    get_arms_user_trace_log_store, text_to_sql)
+    get_arms_user_trace_log_store,
+    text_to_sql,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -27,15 +33,15 @@ class ArmsToolkit:
         @self.server.tool()
         def arms_search_apps(
             ctx: Context,
-            appNameQuery: str = Field(..., description="app name query"),
-            regionId: str = Field(
+            app_name_query: str = Field(..., description="app name query"),
+            region_id: str = Field(
                 ...,
                 description="region id,region id format like 'xx-xxx',like 'cn-hangzhou'",
             ),
-            pageSize: int = Field(
+            page_size: int = Field(
                 20, description="page size,max is 100", ge=1, le=100
             ),
-            pageNumber: int = Field(1, description="page number,default is 1", ge=1),
+            page_number: int = Field(1, description="page number,default is 1", ge=1),
         ) -> list[dict[str, Any]]:
             """搜索ARMS应用。
 
@@ -79,12 +85,12 @@ class ArmsToolkit:
             """
             arms_client: ArmsClient = ctx.request_context.lifespan_context[
                 "arms_client"
-            ].with_region(regionId)
+            ].with_region(region_id)
             request: SearchTraceAppByPageRequest = SearchTraceAppByPageRequest(
-                traceAppName=appNameQuery,
-                regionId=regionId,
-                pageSize=pageSize,
-                pageNumber=pageNumber,
+                trace_app_name=app_name_query,
+                region_id=region_id,
+                page_size=page_size,
+                page_number=page_number,
             )
             response: SearchTraceAppByPageResponse = (
                 arms_client.search_trace_app_by_page(request)
@@ -101,9 +107,9 @@ class ArmsToolkit:
             if page_bean:
                 result["trace_apps"] = [
                     {
-                        "appName": app.app_name,
+                        "app_name": app.app_name,
                         "pid": app.pid,
-                        "userId": app.user_id,
+                        "user_id": app.user_id,
                         "type": app.type,
                     }
                     for app in page_bean.trace_apps
@@ -120,9 +126,9 @@ class ArmsToolkit:
         )
         def arms_generate_trace_query(
             ctx: Context,
-            userId: int = Field(..., description="user aliyun account id"),
+            user_id: int = Field(..., description="user aliyun account id"),
             pid: str = Field(..., description="pid,the pid of the app"),
-            regionId: str = Field(
+            region_id: str = Field(
                 ...,
                 description="region id,region id format like 'xx-xxx',like 'cn-hangzhou'",
             ),
@@ -175,7 +181,7 @@ class ArmsToolkit:
                 包含查询信息的字典，包括sls_query、project和log_store
             """
 
-            data: dict[str, str] = get_arms_user_trace_log_store(userId, regionId)
+            data: dict[str, str] = get_arms_user_trace_log_store(user_id, region_id)
             instructions = [
                 "1. pid为" + pid,
                 "2. 响应时间字段为 duration,单位为纳秒，转换成毫秒",
@@ -192,10 +198,86 @@ class ArmsToolkit:
             请根据以上信息生成sls查询语句
             """
             sls_text_to_query = text_to_sql(
-                ctx, prompt, data["project"], data["logStore"], regionId
+                ctx, prompt, data["project"], data["log_store"], region_id
             )
             return {
-                "slsQuery": sls_text_to_query,
+                "sls_query": sls_text_to_query,
                 "project": data["project"],
-                "logStore": data["logStore"],
+                "log_store": data["log_store"],
             }
+
+        @self.server.tool()
+        def arms_profile_flame_analysis(
+                ctx: Context,
+                service_name: str = Field(..., description="arms service name"),
+                start_ms: str = Field(..., description="profile start ms"),
+                end_ms: str = Field(..., description="profile end ms"),
+                profile_type = Field(..., description="profile type, like 'cpu' 'alloc_in_new_tlab_bytes' 'alloc_in_new_tlab_objects'"),
+                ip: str = Field(..., description="arms service host ip"),
+                language: str = Field(..., description="arms service language, like 'java' 'golang' 'python'"),
+                thread: str = Field(..., description="arms service thread id"),
+                thread_group: str = Field(..., description="arms service thread group"),
+                region_id: str = Field(default=...,
+                    description="aliyun region id,region id format like 'xx-xxx',like 'cn-hangzhou'",
+                ),
+        ) -> dict:
+            """分析ARMS应用火焰图性能热点。
+
+            ## 功能概述
+
+            当应用存在性能问题且开启持续剖析时，可以调用该工具对ARMS应用火焰图性能热点进行分析，生成分析结果。分析结果会包含火焰图的性能热点问题、优化建议等信息。
+
+            ## 使用场景
+
+            - 当需要分析ARMS应用火焰图性能问题时
+
+            ## 查询示例
+
+            - "帮我分析下应用 XXX 的火焰图性能热点"
+
+            Args:
+                ctx: MCP上下文，用于访问SLS客户端
+                service_name: ARMS应用监控服务名称
+                start_ms: 分析的开始时间
+                end_ms: 分析的结束时间
+                profile_type: Profile类型，用于选择需要分析的Profile指标
+                ip: ARMS应用服务主机地址，用于选择所在的服务机器，不填默认所有
+                language: ARMS服务的变成语言，如'java' 'golang' 'python'等
+                thread: 服务线程名称，用于选择对应线程，不填默认所有
+                thread_group: 服务线程组名称，用于选择对应线程组，不填默认所有
+                region_id: 阿里云区域ID
+            """
+            try:
+                sls_client: Client = ctx.request_context.lifespan_context[
+                    "sls_client"
+                ].with_region("cn-shanghai")
+                request: CallAiToolsRequest = CallAiToolsRequest()
+                request.tool_name = "profile_flame_analysis"
+                request.region_id = region_id
+                params: dict[str, Any] = {
+                    "serviceName": service_name,
+                    "startMs": start_ms,
+                    "endMs": end_ms,
+                    "profileType": profile_type,
+                    "ip": ip,
+                    "language": language,
+                    "thread": thread,
+                    "threadGroup": thread_group,
+                    "sys.query": f"帮我分析下应用 {service_name} 的火焰图性能热点问题",
+                }
+                request.params = params
+                runtime: util_models.RuntimeOptions = util_models.RuntimeOptions()
+                runtime.read_timeout = 60000
+                runtime.connect_timeout = 60000
+                tool_response: CallAiToolsResponse = (
+                    sls_client.call_ai_tools_with_options(
+                        request=request, headers={}, runtime=runtime
+                    )
+                )
+                data = tool_response.body
+                if "------answer------\n" in data:
+                    data = data.split("------answer------\n")[1]
+                return data
+            except Exception as e:
+                logger.error(f"调用火焰图数据性能热点AI工具失败: {str(e)}")
+                raise
